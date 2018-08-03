64f18e2b9483a18661488678c067cf8e0460453a
Farrel <Farrel@LAPTOP-RLK1BSK1>
2018-08-03 11:10:31 +0700

    Commit Message : Coba IF..ELSE

--- src/cobapercabangan/CobaPercabangan.java
+++ src/cobapercabangan/CobaPercabangan.java
@@ -37,6 +37,13 @@ public class CobaPercabangan {
             System.out.println("isi variable a : " + a);
             System.out.println("nilai a lebih dari 5");
         }
+        
+        System.out.println("\nIF..ELSE..");
+        if(a < 5) {
+            System.out.println("isi variabel a : " + 5);
+            System.out.println("nilai kurang dari 5");
+        } else System.out.println("nilai a lebih dari atau sama dengan 5");
+        }
     }
     
-}
+
